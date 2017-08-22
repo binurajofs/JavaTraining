9600b6b648b6203151a63995b533bebb73582a24
Kannndev <rbkannan1@gmail.com>
2017-08-22 23:01:58 +0530

    Pattern 5 for loop

---
+++ src/com/ofs/java/kannan/TrianglePattern5.java
@@ -0,0 +1,22 @@
+package com.ofs.java.kannan;
+
+public class TrianglePattern5 {
+
+	public static void main(String[] args) {
+		int n=8;
+		for(int i=1;i<=5;i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			for(int j=1; j<=n; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			n=n-2;
+		}
+	}
+
+}
