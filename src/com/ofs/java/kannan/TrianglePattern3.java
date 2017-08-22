60c278a3e4fd10a554d6f960cffcb6072abd865b
Kannndev <rbkannan1@gmail.com>
2017-08-22 19:21:51 +0530

    Triangle Pattern 3

---
+++ src/com/ofs/java/kannan/TrianglePattern3.java
@@ -0,0 +1,15 @@
+package com.ofs.java.kannan;
+
+public class TrianglePattern3 {
+
+	public static void main(String[] args) {
+		for(int i=4;i>=0;i--) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("* ");
+			}
+		System.out.println("");
+		}
+
+	}
+
+}
