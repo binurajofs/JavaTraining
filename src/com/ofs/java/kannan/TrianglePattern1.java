d875d53f65ab384e6779f126fd47096b6965b4f6
Kannndev <rbkannan1@gmail.com>
2017-08-22 21:46:28 +0530

    Triangle using while

--- src/com/ofs/java/kannan/TrianglePattern1.java
+++ src/com/ofs/java/kannan/TrianglePattern1.java
@@ -3,10 +3,14 @@ package com.ofs.java.kannan;
 public class TrianglePattern1 {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=5;i++) {
-			for(int j=0; j<i;j++) {
-			System.out.print("* ");
+		int i=0;
+		while(i<5) {
+			int j=0;
+			while(j<=i){
+				System.out.print("* ");
+				j++;
 			}
+			i++;
 			System.out.println();
 		}
 
