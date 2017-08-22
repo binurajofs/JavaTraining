a09c5e35781bfdab1c2c74a28953fa3b46f166af
Kannndev <rbkannan1@gmail.com>
2017-08-22 23:20:12 +0530

    Pattern 5

---
+++ src/com/ofs/java/kannan/TrianglePattern6.java
@@ -0,0 +1,24 @@
+package com.ofs.java.kannan;
+
+public class TrianglePattern6 {
+
+	public static void main(String[] args) {
+		int n=0;
+		for(int i=5; i>=1; i--)
+		{
+			for(int j=i; j>=1; j--) {
+				System.out.print("*");
+			}
+			for(int j=1;j<=n; j++) {
+				System.out.print(" ");
+			}
+
+			for(int j=i; j>=1; j--) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			n=n+2;
+		}
+
+	}
+}
