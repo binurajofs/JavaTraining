e22b5824d8947d32cc8810147a9f225b27d656a3
Kannndev <rbkannan1@gmail.com>
2017-08-22 22:09:58 +0530

    triangle 3 using do while

--- src/com/ofs/java/kannan/TrianglePattern3.java
+++ src/com/ofs/java/kannan/TrianglePattern3.java
@@ -3,12 +3,20 @@ package com.ofs.java.kannan;
 public class TrianglePattern3 {
 
 	public static void main(String[] args) {
-		for(int i=4;i>=0;i--) {
-			for(int j=0; j<=i; j++) {
+		//for(int i=4;i>=0;i--) {
+		int i=4;
+		do {
+			//for(int j=0; j<=i; j++) {
+			int j=0;
+			do {
 				System.out.print("* ");
-			}
+				j++;
+			}while(j<=i);
+			//}
 		System.out.println("");
-		}
+		i--;
+		}while(i>=0);
+			//}
 
 	}
 
