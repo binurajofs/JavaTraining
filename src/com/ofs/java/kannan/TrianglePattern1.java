bff44ea02b47866983ac7df626a93393f5e25a6e
Kannndev <rbkannan1@gmail.com>
2017-08-22 18:44:20 +0530

    Triangle Pattern 1 added

---
+++ src/com/ofs/java/kannan/TrianglePattern1.java
@@ -0,0 +1,15 @@
+package com.ofs.java.kannan;
+
+public class TrianglePattern1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=0; j<i;j++) {
+			System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
