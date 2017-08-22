28928e9219dcda7650b509d95e6630ab82d76945
Kannndev <rbkannan1@gmail.com>
2017-08-22 21:57:45 +0530

    triangle 2 using while

--- src/com/ofs/java/kannan/TrianglePattern2.java
+++ src/com/ofs/java/kannan/TrianglePattern2.java
@@ -2,15 +2,26 @@ package com.ofs.java.kannan;
 
 public class TrianglePattern2 {
 	public static void main(String[] args) {
-		int n=5;
-		for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                 System.out.print(" ");
+		int n=5,i=0;
+		//for (int i = 0; i < n; i++) {
+		while(i<n) {
+            //for (int j = 0; j < n - i; j++) {
+            int j=0;
+            while(j<n-i) {
+			System.out.print(" ");
+			j++;
             }
-            for (int k = 0; k <= i; k++) {
+            //}
+            //for (int k = 0; k <= i; k++) {
+            int k=0;
+            while(k<=i) {
                  System.out.print("* ");
+                 k++;
             }
+            //}
             System.out.println();
-       }
+		i++;
+		}
+       //}
 	}
 }
