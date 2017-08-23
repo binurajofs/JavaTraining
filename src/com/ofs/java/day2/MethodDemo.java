b3f5605c6393833a6ce8dc1ff89530d8ac80f0b4
Rajkamal1996 <raj1996kamal@gmail.com>
2017-08-23 15:41:07 +0530

    javaTraining

---
+++ src/com/ofs/java/day2/MethodDemo.java
@@ -0,0 +1,8 @@
+package com.ofs.java.binu.day2;
+
+public class MethodDemo {
+
+	// Create a method to add two number, and print here.
+	
+	// Create a method to add two number, and return the result(use return type)
+}
---
+++ src/com/ofs/java/day2/MethodOverLoadDemo.java
@@ -0,0 +1,11 @@
+package com.ofs.java.binu.day2;
+
+public class MethodOverLoadDemo {
+
+	// create a method to do algebra operations;
+	// Mehtod name should be calculateLHS() and calculateRHS; and calculate RHS and LHS
+	// Pass inputs as parameter;
+	// 1) ac + bc = c(a + b)
+	// 2) a3 + b3 = (a + b)(a2 - ab + b2)
+	// methods with return type;
+}
