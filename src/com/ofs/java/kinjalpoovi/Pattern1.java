c0e152056d0b408b8cba07175b26890590c28cb3
Poovarasan Muralidaran <poovarasan.m@itp.objectfrontier.com>
2017-08-23 17:03:05 +0530

    Pattern1

---
+++ src/com/ofs/java/kinjalpoovi/Pattern1.java
@@ -0,0 +1,17 @@
+package com.ofs.java.kinjalpoovi;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+int i=0,j=0;
+while(i<5)
+{
+while(j<=i) {
+System.out.print("*");
+j++;
+}
+j=0;
+System.out.println();
+i++;
+}	}
+
+}
