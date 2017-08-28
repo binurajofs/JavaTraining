ee94df7828432cfe1e7aa0af5c18060bf7cf8527
Ravivarma Ramadas <ravivarma.r@itp.objectfrontier.com>
2017-08-28 18:47:02 +0530

    Completed

---
+++ src/com/ofs/java/blaze1457/day3/Student/StudentInfo.java
@@ -0,0 +1,34 @@
+package com.ofs.java.blaze1457.day3.Student;
+
+import com.ofs.java.blaze1457.day3.entry.StudentMarkEntity;
+
+/**
+ * StudentInfo Extending StudentDemo class and implementing StudentMarks Interface
+ * @author blaze1457
+ *
+ */
+public class StudentInfo extends StudentDemo implements StudentMarks{
+
+	@Override
+	/**
+	 * calculateMark Overriding
+	 * @param object of StudentMarkEntity
+	 */
+	public void calculateMark(StudentMarkEntity marks) {
+		// TODO Auto-generated method stub
+		try {
+			marks.total=marks.physics+marks.chemistry+marks.maths;
+			marks.average=marks.total/3;
+			System.out.println("Mark in Chemistry is "+marks.getChemistry());
+			System.out.println("Mark in physics is "+marks.getPhysics());
+			System.out.println("Mark in Maths is "+marks.getMaths());
+			System.out.println("total is "+marks.getTotal());
+			System.out.println("Average is "+marks.getAverage());
+			
+		}
+		catch(Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+}
