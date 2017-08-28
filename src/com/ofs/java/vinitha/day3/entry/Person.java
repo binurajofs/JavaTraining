aae27b4fd745ccb1c542d585689ef93a3aed4e94
vini1216 <31240752+vini1216@users.noreply.github.com>
2017-08-28 15:08:45 +0530

    Update Person.java

--- src/com/ofs/java/vinitha/day3/entry/Person.java
+++ src/com/ofs/java/vinitha/day3/entry/Person.java
@@ -1,8 +1,8 @@
-package com.ofs.java.blaze1457.day3.Entry;
+package com.ofs.java.vinitha.day3.Entry;
 
 /**
  * Person Class
- * @author blaze1457
+ * @author vinitha
  *
  */
 public class Person {
