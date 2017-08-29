d5d1520f89cc482aa5540155a817b4d612c7e08c
swathi0595 <31065642+swathi0595@users.noreply.github.com>
2017-08-29 18:40:20 +0530

    Add files via upload

---
+++ Methodemo.java
@@ -0,0 +1,21 @@
+package src.swathiassign.day2;
+
+public class Methodemo {
+	public static void main(String a[]) {
+int addition=0;
+
+
+public void add1(int a,int b)
+{
+	addition= a+b;
+	System.out.println( "addition of two numbers is"+ addition);
+}
+
+public int  add2(int a, int b)
+{
+	addition= a+b;
+	return addition;
+	
+}
+}
+}
