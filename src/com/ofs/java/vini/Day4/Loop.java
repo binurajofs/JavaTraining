199b25175e2627a0361284d80225d510d5a8b69f
unknown <vinitha.r@PC1355.itp.objectfrontier.com>
2017-09-01 18:23:50 +0530

    day 4

---
+++ src/com/ofs/java/vini/Day4/Loop.java
@@ -0,0 +1,22 @@
+package com.ofs.java.vini.Day4;
+
+public class Loop {
+	public static void main(String args[])
+	{
+	//for loop
+	int input = 5;
+	for(int i = 1; i <= input; i++ )
+	{
+	    System.out.println(i);
+	}
+	 
+	//while loop
+	int i = 1;
+	while(i <= input)
+	{
+	    System.out.println(i);
+	    i++;
+	}
+	}
+
+}
