6dfe1e1cd500d38b73b15fcfe3a6a980adebb67a
unknown <vinitha.r@PC1355.itp.objectfrontier.com>
2017-09-02 10:31:04 +0530

    day4

---
+++ src/com/ofs/java/vini/patterns/Grade.java
@@ -0,0 +1,40 @@
+package com.ofs.java.vini.patterns;
+import java.util.Scanner;
+public class Grade {
+
+	
+		private static Scanner in;
+
+		/*This class reads scores of a student in each exam and asssigns a grade based on average of marks*/
+		public static void main(String[] args){
+			int quiz;
+			int mid_term;
+			int final_score;
+			float avg;
+			
+			in = new Scanner(System.in);
+			System.out.println("Enter score for quiz:");
+			quiz=in.nextInt();
+			System.out.println("Enter score for mid term:");
+			mid_term=in.nextInt();
+			System.out.println("Enter score for final");
+			final_score=in.nextInt();
+			avg=(quiz+mid_term+final_score)/3;
+			if (avg>=90){
+				 System.out.println("Your grade is:A");
+			}
+			else if(avg>=70 && avg< 90){
+				System.out.println("Your grade is:B");
+			}
+			else if(avg>=50 && avg< 70){
+				System.out.println("Your grade is:C");
+			}
+			else if (avg<50){
+				System.out.println("Your grade is:F");
+			}
+			
+		}
+
+	}
+
+
